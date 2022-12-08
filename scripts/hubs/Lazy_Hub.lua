2dc3f33565d92840e92f45245760aa025cd3e99f
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2022-12-08 04:28:06 -0800

    Lazy Hub
    added lazy Hub

---
+++ scripts/hubs/Lazy_Hub.lua
@@ -0,0 +1,15 @@
+--[[
+ ▄▄ • ▪  ▄▄▄▄▄ ▄ .▄▄• ▄▌▄▄▄▄·    ▄▄·       • ▌ ▄ ·.     .▄▄ · ▄▄▄▄▄▄▄▄  ▄▄▄ .▄▄▄ .▄ •▄  ▄▄▄· ▪  ·▄▄▄▄•
+▐█ ▀ ▪██ •██  ██▪▐██▪██▌▐█ ▀█▪  ▐█ ▌▪ ▄█▀▄ ·██ ▐███▪    ▐█ ▀. •██  ▀▄ █·▀▄.▀·▀▄.▀·█▌▄▌▪▐█ ▀█ ██ ▪▀·.█▌
+▄█ ▀█▄▐█· ▐█.▪██▀▀██▌▐█▌▐█▀▀█▄  ██ ▄▄▐█▌.▐▌▐█ ▌▐▌▐█·    ▄▀▀▀█▄ ▐█.▪▐▀▀▄ ▐▀▀▪▄▐▀▀▪▄▐▀▀▄·▄█▀▀█ ▐█·▄█▀▀▀•
+▐█▄▪▐█▐█▌ ▐█▌·██▌▐▀▐█▄█▌██▄▪▐█  ▐███▌▐█▌.▐▌██ ██▌▐█▌    ▐█▄▪▐█ ▐█▌·▐█•█▌▐█▄▄▌▐█▄▄▌▐█.█▌▐█▪ ▐▌▐█▌█▌▪▄█▀
+·▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ · ▀▀▀ ·▀▀▀▀ ▀ ·▀▀▀  ▀█▄▀▪▀▀  █▪▀▀▀     ▀▀▀▀  ▀▀▀ .▀  ▀ ▀▀▀  ▀▀▀ ·▀  ▀ ▀  ▀ ▀▀▀·▀▀▀ • (z)
+]]
+
+
+repeat task.wait() until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LioK251/RbScripts/main/lazyhub.lua"))()
+--[[ Games
+    Project Slayers, Demon Soul Simulator, A0PG, Anime Rifts, A Universal Time, God's Legacy Demo, Demonfall, Project Baki 2, Floppa Simulator, Reaper 2, Stand Upright : Rebooted.
+]]
