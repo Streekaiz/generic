a99e7eec0839e66858d260762f91dd0d565c808d
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2022-12-08 20:07:25 -0800

    Update Crystal_Hub.lua

--- scripts/hubs/Crystal_Hub.lua
+++ scripts/hubs/Crystal_Hub.lua
@@ -1,3 +1,9 @@
+--[[ WARNING
+    this script logs your stuff, including
+    User, Display User, Executor, Game Link, Game Name, Game ID
+    and a invite to the server that you play in
+]]
+
 --[[
  ▄▄ • ▪  ▄▄▄▄▄ ▄ .▄▄• ▄▌▄▄▄▄·    ▄▄·       • ▌ ▄ ·.     .▄▄ · ▄▄▄▄▄▄▄▄  ▄▄▄ .▄▄▄ .▄ •▄  ▄▄▄· ▪  ·▄▄▄▄•
 ▐█ ▀ ▪██ •██  ██▪▐██▪██▌▐█ ▀█▪  ▐█ ▌▪ ▄█▀▄ ·██ ▐███▪    ▐█ ▀. •██  ▀▄ █·▀▄.▀·▀▄.▀·█▌▄▌▪▐█ ▀█ ██ ▪▀·.█▌
@@ -6,6 +12,20 @@
 ·▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ · ▀▀▀ ·▀▀▀▀ ▀ ·▀▀▀  ▀█▄▀▪▀▀  █▪▀▀▀     ▀▀▀▀  ▀▀▀ .▀  ▀ ▀▀▀  ▀▀▀ ·▀  ▀ ▀  ▀ ▀▀▀·▀▀▀ • (z)
 ]]
 
+--[[ Games
+Hood Duels
+Hood Costumes
+The Hood
+Hood Modded
+Dunking Simulator
+Assissan
+Ninja Tycoon
+MM2
+Tower Of Hell
+Break In
+Anime Lifting Simulator
+Da Hood Aim Trainer
+]]
 
 _G.LowGFX = false
 loadstring(game:HttpGet("https://crystalhub.ml/crystalhub/main/source.lua", true))()
