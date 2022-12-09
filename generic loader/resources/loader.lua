00287bb8a9362bbdc21e6baecdc3520f3035786e
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2022-12-08 19:34:49 -0800

    Update loader.lua

--- generic loader/resources/loader.lua
+++ generic loader/resources/loader.lua
@@ -4,7 +4,7 @@ local Games = {
 
 for _, v in pairs(Games) do
     if game.PlaceId ~= v then
-        warn("dummy")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Streekaiz/generic/main/generic%20loader/games/Universal.lua", true))()
         else
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Streekaiz/generic/main/generic%20loader/games/"..game.PlaceId..".lua", true))()
     end
