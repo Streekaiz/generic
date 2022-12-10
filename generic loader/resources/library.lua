8f6bbb913386b2f3744eb0e72cb01e56315089bc
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2022-12-09 20:25:08 -0800

    Update library.lua

--- generic loader/resources/library.lua
+++ generic loader/resources/library.lua
@@ -17,14 +17,6 @@ local OrionLib = {
 	Flags = {},
 	Themes = {
 		Default = {
-			Main = Color3.fromRGB(25, 25, 25),
-			Second = Color3.fromRGB(32, 32, 32),
-			Stroke = Color3.fromRGB(60, 60, 60),
-			Divider = Color3.fromRGB(60, 60, 60),
-			Text = Color3.fromRGB(240, 240, 240),
-			TextDark = Color3.fromRGB(150, 150, 150)
-		},
-		Thing = {
 			Main = Color3.fromRGB(255, 25, 25),
 			Second = Color3.fromRGB(32, 32, 32),
 			Stroke = Color3.fromRGB(60, 60, 60),
@@ -33,7 +25,7 @@ local OrionLib = {
 			TextDark = Color3.fromRGB(150, 150, 150)
 		}
 	},
-	SelectedTheme = "Thing",
+	SelectedTheme = "Default",
 	Folder = nil,
 	SaveCfg = false
 }
