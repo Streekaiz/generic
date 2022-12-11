995e66c253bb4ee31913b9feff9dc616bd40f8cc
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2022-12-10 22:15:30 -0800

    Update Universal.lua

--- generic loader/games/Universal.lua
+++ generic loader/games/Universal.lua
@@ -1,20 +1,3 @@
-local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))() --[[
-	its simple, easy to use, and looks decent
-	stfu
-]]
-
-local Old = {
-    WS = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed or game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed or 16,
-    JP = game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower or game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").JumpPower or 50,
-    G = workspace.Gravity or 196,
-	FOV = game.workspace.CurrentCamera.FieldOfView or 70,
-	A = game:GetService("Lighting").Ambient,
-	B = game:GetService("Lighting").Brightness,
-    Name = game:GetService("Players").LocalPlayer.Name,
-    Display = game:GetService("Players").LocalPlayer.DisplayName,
-}
-
-
 local gmt = getrawmetatable(game)
 local oldindex = gmt.__index
 setreadonly(gmt, false)
@@ -25,6 +8,12 @@ gmt.__index = newcclosure(function(Self, Key)
     if tostring(Self) == "Humanoid" and tostring(Key) == "JumpPower" then
         return
     end
+	if tostring(Self) == "Humanoid" and tostring(Key) == "AutoRotate" then
+		return
+	end
+	if tostring(Self) == "Humanoid" and tostring(Key) == "HipHeight" then
+		return
+	end
     if tostring(Key) == "Gravity" then
         return
     end
@@ -32,9 +21,28 @@ gmt.__index = newcclosure(function(Self, Key)
 end)
 
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Streekaiz/generic/main/generic%20loader/resources/library.lua')))()
+getgenv().PlayerAmount = 0
+
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))() --[[
+	its simple, easy to use, and looks decent
+	stfu
+]]
+local Camera = workspace.CurrentCamera
+local Old = {
+    WS = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed or game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed or 16,
+    JP = game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower or game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").JumpPower or 50,
+    G = workspace.Gravity or 196,
+	FOV = game.workspace.CurrentCamera.FieldOfView or 70,
+	A = game:GetService("Lighting").Ambient,
+	B = game:GetService("Lighting").Brightness,
+	HH = game:GetService("Players").LocalPlayer.Character.Humanoid.HipHeight or game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").HipHeight or 2,
+    Name = game:GetService("Players").LocalPlayer.Name,
+    Display = game:GetService("Players").LocalPlayer.DisplayName,
+}
+
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Streekaiz/generic/main/generic%20loader/resources/library.lua', true))()
 local Window = OrionLib:MakeWindow({
-    Name = "universal v1 | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, 
+    Name = "universal v1.3 | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, 
     HidePremium = true,
 	ConfigFolder = "generic_cfg",
     SaveConfig = true,
@@ -47,8 +55,9 @@ local Tab = {
     Home = Window:MakeTab({ Name = "Home", Icon = "rbxassetid://9792650361", PremiumOnly = false }),
     Player = Window:MakeTab({ Name = "Player", Icon = "rbxassetid://9792631906", PremiumOnly = false }),
     Render = Window:MakeTab({ Name = "Rendering", Icon = "rbxassetid://9792631281", PremiumOnly = false }),
-    Scripts = Window:MakeTab({ Name = "Scripts", Icon = "rbxassetid://6034684949", PremiumOnly = false }),
-    Misc = Window:MakeTab({ Name = "Miscallaenous", Icon = "rbxassetid://9792633222", PremiumOnly = false }),
+	Combat = Window:MakeTab({ Name = "Combat", Icon = "rbxassetid://4483345998", PremiumOnly = false }),
+	Scripts = Window:MakeTab({ Name = "Scripts", Icon = "rbxassetid://6034684949", PremiumOnly = false }),
+	Misc = Window:MakeTab({ Name = "Miscallaenous", Icon = "rbxassetid://9792633222", PremiumOnly = false }),
 }
 
 Tab.Home:AddSection({
@@ -77,6 +86,7 @@ Tab.Player:AddSection({
 local OWSToggle = Tab.Player:AddToggle({ -- thanks flags! 
 	Name = "Walk Speed",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "WST",
 	Save = true,
 	Callback = function(Value)
@@ -89,6 +99,7 @@ local OWSToggle = Tab.Player:AddToggle({ -- thanks flags!
 local OJPToggle = Tab.Player:AddToggle({
 	Name = "Jump Power",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "JPT",
 	Save = true,
 	Callback = function(Value)
@@ -101,6 +112,7 @@ local OJPToggle = Tab.Player:AddToggle({
 local OGToggle = Tab.Player:AddToggle({
 	Name = "Gravity",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "GT",
 	Save = true,
 	Callback = function(Value)
@@ -113,7 +125,7 @@ local OGToggle = Tab.Player:AddToggle({
 local OFOVToggle = Tab.Player:AddToggle({
 	Name = "Field Of View",
 	Default = false,
-	Color = Color3.fromRGB(230, 67, 77),
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "FOVT",
 	Save = true,
 	Callback = function(Value)
@@ -134,7 +146,7 @@ Tab.Player:AddSlider({
 	Min = 0,
 	Max = 150,
 	Default = Old.WS,
-	Color = Color3.fromRGB(44, 118, 222),
+	Color = Color3.fromRGB(118, 60, 232),
 	Increment = 1,
 	ValueName = "%",
 	Flag = "WSV",
@@ -148,7 +160,7 @@ Tab.Player:AddSlider({
 	Min = 0,
 	Max = 150,
 	Default = Old.JP,
-	Color = Color3.fromRGB(44, 118, 222),
+	Color = Color3.fromRGB(118, 60, 232),
 	Increment = 1,
 	ValueName = "%",
 	Flag = "JPV",
@@ -162,7 +174,7 @@ Tab.Player:AddSlider({
 	Min = 0,
 	Max = 196,
 	Default = Old.G,
-	Color = Color3.fromRGB(44, 118, 222),
+	Color = Color3.fromRGB(118, 60, 232),
 	Increment = 1,
 	ValueName = "%",
 	Flag = "GV",
@@ -176,7 +188,7 @@ Tab.Player:AddSlider({
 	Min = 0,
 	Max = 120,
 	Default = Old.FOV,
-	Color = Color3.fromRGB(230, 67, 77),
+	Color = Color3.fromRGB(118, 60, 232),
 	Increment = 1,
 	ValueName = "%",
 	Flag = "FOVV",
@@ -192,7 +204,7 @@ Tab.Render:AddSection({
 Tab.Render:AddToggle({
 	Name = "ESP",
 	Default = false,
-	Color = Color3.fromRGB(93, 222, 134),
+	Color = Color3.fromRGB(106, 47, 222),
 	Flag = "ESPT",
 	Save = true,
 	Callback = function(Value)
@@ -211,7 +223,7 @@ Tab.Render:AddToggle({
 Tab.Render:AddToggle({
 	Name = "Boxes",
 	Default = false,
-	Color = Color3.fromRGB(93, 222, 134),
+	Color = Color3.fromRGB(106, 47, 222),
 	Flag = "ESPB",
 	Save = true,
 	Callback = function(Value)
@@ -222,7 +234,7 @@ Tab.Render:AddToggle({
 Tab.Render:AddToggle({
 	Name = "Tracers",
 	Default = false,
-	Color = Color3.fromRGB(93, 222, 134),
+	Color = Color3.fromRGB(106, 47, 222),
 	Flag = "ESPT",
 	Save = true,
 	Callback = function(Value)
@@ -233,7 +245,7 @@ Tab.Render:AddToggle({
 Tab.Render:AddToggle({
 	Name = "Names & Distance",
 	Default = false,
-	Color = Color3.fromRGB(93, 222, 134),
+	Color = Color3.fromRGB(106, 47, 222),
 	Flag = "ESPND",
 	Save = true,
 	Callback = function(Value)
@@ -248,7 +260,7 @@ Tab.Render:AddSection({
 Tab.Render:AddToggle({
 	Name = "Face Camera",
 	Default = false,
-	Color = Color3.fromRGB(57, 219, 108),
+	Color = Color3.fromRGB(104, 41, 227),
 	Flag = "ESPFC",
 	Save = true,
 	Callback = function(Value)
@@ -262,14 +274,15 @@ Tab.Render:AddSection({
 
 Tab.Render:AddColorpicker({
 	Name = "ESP Color",
-	Default = Color3.fromRGB(93, 222, 134), -- funy
+	Default = Color3.fromRGB(112, 49, 235), -- funy
 	Flag = "ESPC",
 	Save = true,
 	Callback = function(Value)
 		ESP.Color = Value
 	end	  
 })
---[[
+
+Tab.Combat:AddLabel("theres more shit below just scroll")
 Tab.Combat:AddSection({
 	Name = "Aimbot"
 })
@@ -281,6 +294,7 @@ Tab.Combat:AddSection({
 Tab.Combat:AddToggle({
 	Name = "Enabled",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "SAET",
 	Save = true,
 	Callback = function(Value)
@@ -290,6 +304,7 @@ Tab.Combat:AddToggle({
 Tab.Combat:AddToggle({
 	Name = "Team Check",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "SATCT",
 	Save = true,
 	Callback = function(Value)
@@ -299,6 +314,7 @@ Tab.Combat:AddToggle({
 Tab.Combat:AddToggle({
 	Name = "Wall Check",
 	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
 	Flag = "SAWCT",
 	Save = true,
 	Callback = function(Value)
@@ -330,7 +346,71 @@ Tab.Combat:AddSlider({
 	Min = 0,
 	Max = 100,
 	Default = 100,
-	Color = Color3.fromRGB(255,255,255),
+	Flag = "SHC",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
+	Increment = 1,
+	ValueName = "%",
+	Callback = function(Value)
+	end    
+})
+
+Tab.Combat:AddSection({
+	Name = "Anti Aim Settings"
+})
+
+Tab.Combat:AddToggle({
+	Name = "Anti Aim",
+	Default = false,
+	Flag = "AntiAim",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
+	Callback = function(Value)
+	end    
+})
+
+Tab.Combat:AddDropdown({
+	Name = "Yaw Base",
+	Default = "Camera",
+	Flag = "YAWBASD",
+	Save = true,
+	Options = {"Camera", "Spin", "Random"},
+	Callback = function(Value)
+	end    
+})
+
+Tab.Combat:AddDropdown({
+	Name = "Yaw Modifier",
+	Default = "None",
+	Flag = "YAWMODD",
+	Save = true,
+	Options = {"None", "Jitter", "Offset Jitter"},
+	Callback = function(Value)
+	end    
+})
+
+Tab.Combat:AddSlider({
+	Name = "yaw modifier",
+	Min = -180,
+	Max = 180,
+	Default = 0,
+	Flag = "YAWOFFMODV",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
+	Increment = 1,
+	ValueName = "%",
+	Callback = function(Value)
+	end    
+})
+ -- i mixed up the flags
+Tab.Combat:AddSlider({
+	Name = "yaw offset modifier",
+	Min = -180,
+	Max = 180,
+	Default = 0,
+	Flag = "YAWMODV",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
 	Increment = 1,
 	ValueName = "%",
 	Callback = function(Value)
@@ -344,21 +424,54 @@ Tab.Combat:AddSection({
 Tab.Combat:AddSection({
 	Name = "Silent Aim FOV"
 })
-]]
+
+Tab.Combat:AddColorpicker({
+	Name = "FOV Circle Color",
+	Default = Color3.fromRGB(118, 60, 232),
+	Flag = "SILENTAIMFOVCIRCLE",
+	Save = true,
+	Callback = function(Value)
+	end
+})
+
+Tab.Combat:AddSlider({
+	Name = "Radius",
+	Min = 0,
+	Max = 360,
+	Default = 50,
+	Flag = "SILENTAIMFOVRADIUS",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
+	Increment = 1,
+	ValueName = "%",
+	Callback = function(Value)
+	end    
+})
+
+Tab.Combat:AddSlider({
+	Name = "Sides",
+	Min = 0,
+	Max = 500,
+	Default = 100,
+	Flag = "SILENTAIMSIDESRADIUS",
+	Save = true,
+	Color = Color3.fromRGB(118, 60, 232),
+	Increment = 1,
+	ValueName = "%",
+	Callback = function(Value)
+	end    
+})
+
 Tab.Scripts:AddSection({
 	Name = "Load Scripts"
 })
 local Universal_To_Load = "None"
 local Game_To_Load = "None"
-local Link = {
-	Universal = "https://raw.githubusercontent.com/Streekaiz/generic/main/scripts/universal/",
-	Games = "https://raw.githubusercontent.com/Streekaiz/generic/main/scripts/regular%20scripts/"
-}
 local Game_Dropdown = Tab.Scripts:AddDropdown({
 	Name = "Select Game",
 	Default = "None",
 	Flag = "Game_Dropdown",
-	Options = {"None", "Anime_Adventures.lua", "Bedwars.lua", "Bee_Swarm_Simulator.lua", "Combat_Warriors.lua", "Dark_Life.lua", "Doors.lua", "Fruit_BattleGrounds.lua", "Jailbreak.lua", "Labyrinth.lua", 'Mount_Of_The_Gods.lua', "Murder_Mystery_2.lua", "My_Resturant.lua", "Neon_Knights.lua", "PLS_Donate.lua", "Pet_Simulator_X.lua", "Road_To_Gamby.lua", "SKYWARS.lua", "Site_76.lua", "Stand_Upright_Rebooted.lua", "Wind_Of_Fortune.lua", "World_Zero.lua"},
+	Options = {"None", "Anime_Adventures.lua", "Bedwars.lua", "Busy_Buisness.lua", "Bee_Swarm_Simulator.lua", "Combat_Warriors.lua", "Dark_Life.lua", "Doors.lua", "Fruit_BattleGrounds.lua", "Jailbreak.lua", "Labyrinth.lua", 'Mount_Of_The_Gods.lua', "Murder_Mystery_2.lua", "My_Resturant.lua", "Neon_Knights.lua", "PLS_Donate.lua", "Pet_Simulator_X.lua", "Road_To_Gamby.lua", "SKYWARS.lua", "Site_76.lua", "Stand_Upright_Rebooted.lua", "Wind_Of_Fortune.lua", "World_Zero.lua"},
 	Callback = function(Value)
 	Game_To_Load = Value
 	end    
@@ -368,7 +481,7 @@ local Universal_Dropdown = Tab.Scripts:AddDropdown({
 	Name = "Select Universal Script",
 	Default = "None",
 	Flag = "Universal_Dropdown",
-	Options = {"None", "CMD_X.lua", "Domain_X.lua", "Fates_Admin.lua", "GPU_Saver.lua", "Infinite_Yield.lua", "Orca_Hub.lua", "Player_Radar.lua", "Silent_Aim.lua", "Unnamed_ESP.lua", "Vape.lua"},
+	Options = {"None", "CMD_X.lua", "Domain_X.lua", "Fates_Admin.lua", "GPU_Saver.lua", "Infinite_Yield.lua", "Orca_Hub.lua", "Player_Radar.lua", "Silent_Aim.lua", "Unnamed_ESP.lua", "Vape.lua", "Hitbox_Expander.lua"},
 	Callback = function(Value)
 	Universal_To_Load = Value
 	end    
@@ -451,12 +564,31 @@ Tab.Scripts:AddButton({
   	end    
 })
 
+Tab.Misc:AddSection({
+	Name = "UI Settings"
+})
+
 Tab.Misc:AddButton({
 	Name = "Unload UI",
 	Callback = function()
       	OrionLib:Destroy()
   	end    
 })
+
+Tab.Misc:AddSection({
+	Name = "Info"
+})
+
+local SERAGELABEL = Tab.Misc:AddLabel("Server Age:"..math.floor(workspace.DistributedGameTime / 60 / 60).." "..math.floor(workspace.DistributedGameTime / 60).." "..math.floor(workspace.DistributedGameTime))
+local PLRAMOUNTLABEL = Tab.Misc:AddLabel("Players: "..getgenv().PlayerAmount)
+
+	for _, v in pairs(game:GetService("Players"):GetPlayers()) do
+		getgenv().PlayerAmount = getgenv().PlayerAmount + 1
+		pcall(function() PLRAMOUNTLABEL:Set("Players: "..getgenv().PlayerAmount) end)
+	end
+	game:GetService("Players").PlayerAdded:Connect(function() getgenv().PlayerAmount = getgenv().PlayerAmount + 1 pcall(function() PLRAMOUNTLABEL:Set("Players: "..getgenv().PlayerAmount) end) end)
+	game:GetService("Players").PlayerRemoving:Connect(function() getgenv().PlayerAmount = getgenv().PlayerAmount - 1 pcall(function() PLRAMOUNTLABEL:Set("Players: "..getgenv().PlayerAmount) end) end)
+
 
 Tab.Misc:AddSection({
 	Name = "Teleport Service"
@@ -475,29 +607,108 @@ Tab.Misc:AddButton({
       	game:GetService("TeleportService"):Teleport(game.PlaceId)
   	end    
 })
+
+Tab.Misc:AddSection({
+	Name = "Lighting"
+})
+
+Tab.Misc:AddToggle({
+	Name = "Ambience",
+	Default = false,
+	Color = Color3.fromRGB(118, 60, 232),
+	Flag = "AMBTOG",
+	Save = true,
+	Callback = function(Value)
+	    if Value ~= true then
+			game:GetService("Lighting").Ambient = Old.A
+			else
+			    game:GetService("Lighting").Ambient = OrionLib.Flags['AMBCOL'].Value
+		end
+	end
+})
+
+Tab.Misc:AddToggle({
+	Name = "Global Shadows",
+	Default = true,
+	Color = Color3.fromRGB(118, 60, 232),
+    Flag = "GLOBSHADOWS",
+	Save = true,
+	Callback = function(Value)
+		game:GetService("Lighting").GlobalShadows = Value
+	end    
+})
+
+Tab.Misc:AddColorpicker({
+	Name = "Ambience Color",
+	Default = Color3.fromRGB(118, 60, 232),
+	Flag = "AMBCOL",
+	Save = true,
+	Callback = function(Value)
+	    if OrionLib.Flags["AMBTOG"].Value then
+			game:GetService("Lighting").Ambient = Value
+			game:GetService("Lighting").OutdoorAmbient = Value
+		end
+	end
+})
+
 OrionLib:MakeNotification({ Name = "Hey "..Old.Name.."!", Content = "Come join our discord server, it's already been set to your clipboard.", Image = "rbxassetid://9838877673", Time = 5 })
 
 
 game:GetService("RunService").RenderStepped:Connect(function()
 
-        if OrionLib.Flags['WST'].Value then
-    		game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = OrionLib.Flags['WSV'].Value
-		end
+    if OrionLib.Flags['WST'].Value then
+    	game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = OrionLib.Flags['WSV'].Value
+	end
 
-		if OrionLib.Flags['JPT'].Value then
-			game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = OrionLib.Flags['JPV'].Value
-		end
+	if OrionLib.Flags['JPT'].Value then
+		game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = OrionLib.Flags['JPV'].Value
+	end
 
-		if OrionLib.Flags['GT'].Value then
-			cloneref(workspace).Gravity = OrionLib.Flags['GV'].Value
-		end
+	if OrionLib.Flags['GT'].Value then
+		cloneref(workspace).Gravity = OrionLib.Flags['GV'].Value
+	end
 
-		if OrionLib.Flags['FOVT'].Value then
-			game.workspace.CurrentCamera.FieldOfView = OrionLib.Flags['FOVV'].Value
-		end
+	if OrionLib.Flags['FOVT'].Value then
+		game.workspace.CurrentCamera.FieldOfView = OrionLib.Flags['FOVV'].Value
+	end
+	pcall(function() -- why do i need to do this lol..
+		SERAGELABEL:Set("Hours: "..math.floor(workspace.DistributedGameTime / 60 / 60)..", Minutes: "..math.floor(workspace.DistributedGameTime / 60)..", Seconds: "..math.floor(workspace.DistributedGameTime))
+	end)
+
+
+    local SelfRootPart, SelfHumanoid = game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+    if not game:GetService("Players").LocalPlayer.Character or not SelfRootPart or not SelfHumanoid then return end
+    if OrionLib.Flags["AntiAim"].Value then
+    game:GetService("Players").LocalPlayer.Character.Humanoid.AutoRotate = false
+        local Angle do
+            Angle = -math.atan2(Camera.CFrame.LookVector.Z, Camera.CFrame.LookVector.X) + math.rad(-90)
+            if OrionLib.Flags['YAWBASD'].Value == "Random" then
+                Angle = -math.atan2(Camera.CFrame.LookVector.Z, Camera.CFrame.LookVector.X) + math.rad(math.random(0, 360))
+            elseif OrionLib.Flags['YAWBASD'].Value == "Spin" then
+                Angle = -math.atan2(Camera.CFrame.LookVector.Z, Camera.CFrame.LookVector.X) + tick() * 10 % 360
+            end
+        end
+
+        Jitter = not Jitter
+		local Offset = math.rad(OrionLib.Flags['YAWOFFMODV'].Value)
+        if Jitter then
+            if OrionLib.Flags['YAWMODD'].Value == "Jitter" then
+                Offset = math.rad(OrionLib.Flags['YAWMODV'].Value)
+            elseif OrionLib.Flags['YAWMODD'].Value == "Offset Jitter" then
+                Offset = Offset + math.rad(OrionLib.Flags['YAWMODV'].Value)
+            end
+        end
+        local NewAng = CFrame.new(SelfRootPart.Position) * CFrame.Angles(0, Angle + Offset, 0)
+        local function YRotate(_CFrame)
+            local X, Y, Z = _CFrame:ToOrientation()
+            return CFrame.new(_CFrame.Position) * CFrame.Angles(0, Y, 0)
+        end
+        AntiaimAngle = Angle + Offset
+        SelfRootPart.CFrame = YRotate(NewAng)
+        else
+			SelfHumanoid.AutoRotate = true
+    end
+
 
 end)
-
-
-
 OrionLib:Init()
